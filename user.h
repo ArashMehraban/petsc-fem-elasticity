3c5c81757450c0363b01f48d5087f63c80a9a3c1
ArashMehraban <arashm81@gmail.com>
2017-09-18 23:59:24 -0600

    fixed errors

--- user.h
+++ user.h
@@ -1,16 +1,16 @@
 #include<petscdmplex.h>
 /*for dm and dmplex objects*/
 
-typedef struct ApplicationContext_private *AppCtx;
 
-struct ApplicationContext_private{
+typedef struct{
   char          filename[PETSC_MAX_PATH_LEN]; /*optional exodusII file*/
   PetscBool	    interpolate;                  /*Generate intermediate mesh points: nodes on faces, edges in addition to vertices and element*/
-  PetscInt      polydegree;          /*number of quadrature points in 1D*/
-  PetscInt      dof;                          /*number of degrees of freedom at each node, e.g. For 3D elasticity: at each node we have u1, u2 and u3 */
-};
+  PetscInt      polydegree;          /*polynomial degree (1 less than number of quadrature points in 1D)*/
+  PetscInt      dof;                 /*number of degrees of freedom at each node, e.g. For 3D elasticity: at each node we have u1, u2 and u3 */
+}AppCtx;
 
-PetscErrorCode processUserOptions(MPI_Comm comm, AppCtx userOptions);
-PetscErrorCode dmMeshSetup(MPI_Comm comm, AppCtx user, DM *dm);
-// Use this function after calling dmMeshSetup 
-PetscErrorCode drawEleme(MPI_Comm comm, DM *dm);
+PetscErrorCode processUserOptions(MPI_Comm comm, AppCtx *userOptions);
+PetscErrorCode dmMeshSetup(MPI_Comm comm, AppCtx *user, DM *dm);
+/* Use this function after calling dmMeshSetup.
+   It is best to use this function with one element to see its connectivity */
+PetscErrorCode drawOneElem(DM dm,AppCtx *user);
