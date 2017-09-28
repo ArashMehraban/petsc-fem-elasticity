a9a0e7b1abf5e7147ba0ffe72afc0fd2d273467a
ArashMehraban <arashm81@gmail.com>
2017-09-27 21:34:52 -0600

    Added math.h for mod. Added user defined number of elements (ne) to specialize over

--- user.h
+++ user.h
@@ -4,13 +4,15 @@
 #include<petscdmplex.h>
 /*for dm and dmplex objects*/
 #include "fe.h"
+#include "math.h"
 
 
 typedef struct{
   char          filename[PETSC_MAX_PATH_LEN]; /*optional exodusII file*/
   PetscBool	    interpolate;                  /*Generate intermediate mesh points: nodes on faces, edges in addition to vertices and element*/
-  PetscInt      polydegree;          /*polynomial degree (1 less than number of quadrature points in 1D)*/
-  PetscInt      dof;                 /*number of degrees of freedom at each node, e.g. For 3D elasticity: at each node we have u1, u2 and u3 */
+  PetscInt      polydegree;                   /*polynomial degree (1 less than number of quadrature points in 1D)*/
+  PetscInt      dof;                          /*number of degrees of freedom at each node, e.g. For 3D elasticity: at each node we have u1, u2 and u3 */
+  PetscInt      ne;                           /*number of elements to extract per iteration*/
   PetscInt      *conn;
 }AppCtx;
 
