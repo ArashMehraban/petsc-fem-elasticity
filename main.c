766ba562c6fa0deff4089766773326da0403f9c8
ArashMehraban <arashm81@gmail.com>
2017-09-30 17:50:39 -0600

    update

--- main.c
+++ main.c
@@ -26,27 +26,42 @@ int main(int argc, char **argv)
   PetscErrorCode    ierr;
   DM                dm;
   AppCtx	          user; /*user-defined work context*/
-  Vec               exactSol;
+  Vec               exactSol, Ul; //res
   FE                fe;
+  PetscInt          sz_Ul;
+  const PetscScalar *u;
+
 
   ierr = PetscInitialize(&argc,&argv,(char*)0,help); CHKERRQ(ierr);
-
-  //ierr = MPI_Comm_rank(PETSC_COMM_WORLD, &rank); CHKERRQ(ierr);
-  //ierr = PetscPrintf(PETSC_COMM_SELF,"Hello from process: %d\n", rank);CHKERRQ(ierr);
 
   ierr = processUserOptions(PETSC_COMM_WORLD, &user);CHKERRQ(ierr);
   ierr = dmMeshSetup(PETSC_COMM_WORLD, &user, &dm);CHKERRQ(ierr);
   //ierr = drawOneElem(dm,&user);CHKERRQ(ierr);
-  ierr = DMSetApplicationContext(dm, &user);CHKERRQ(ierr);
+
 
   ierr = DMGetApplicationContext(dm, &fe);CHKERRQ(ierr);
-  //ierr = PetscPrintf(PETSC_COMM_SELF,"fe->polydegree: %d\n", fe->polydegree);CHKERRQ(ierr);
+  ierr = PetscPrintf(PETSC_COMM_SELF,"fe->polydegree: %d\n", fe->polydegree);CHKERRQ(ierr);
 
   ierr = DMCreateGlobalVector(dm, &exactSol);CHKERRQ(ierr);
-  //VecView(u,PETSC_VIEWER_STDOUT_WORLD);
 
   ierr = computeExact(dm, exactSol);
   //VecView(exactSol,PETSC_VIEWER_STDOUT_WORLD);
+
+  //ierr = DMCreateGlobalVector(dm, &res);CHKERRQ(ierr);
+
+  ierr = DMGetLocalVector(dm,&Ul);CHKERRQ(ierr);
+  ierr = VecGetLocalSize(Ul,&sz_Ul);CHKERRQ(ierr);
+  ierr = VecGetArrayRead(Ul,&u);CHKERRQ(ierr);
+  //VecGetLocalSize(Ul,&sz_Ul);
+     //ierr = PetscPrintf(PETSC_COMM_SELF,"sz_Ul %d\n",sz_Ul);CHKERRQ(ierr);
+  //VecView(Ul,PETSC_VIEWER_STDOUT_SELF);
+
+  // PetscScalar *ue;
+  // ierr = dmExtractElems(dm, u, sz_Ul, 1, 4, ue);CHKERRQ(ierr);
+
+  ierr = VecDestroy(&exactSol);CHKERRQ(ierr);
+  ierr = VecDestroy(&Ul);CHKERRQ(ierr);
+
 
 
   ierr = PetscFinalize();CHKERRQ(ierr);
