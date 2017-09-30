dd06e3b88100317e68cf497d21f720e23419c79d
ArashMehraban <arashm81@gmail.com>
2017-09-30 17:51:50 -0600

    updated with user.c

--- user.h
+++ user.h
@@ -11,9 +11,11 @@ typedef struct{
   char          filename[PETSC_MAX_PATH_LEN]; /*optional exodusII file*/
   PetscBool	    interpolate;                  /*Generate intermediate mesh points: nodes on faces, edges in addition to vertices and element*/
   PetscInt      polydegree;                   /*polynomial degree (1 less than number of quadrature points in 1D)*/
-  PetscInt      dof;                          /*number of degrees of freedom at each node, e.g. For 3D elasticity: at each node we have u1, u2 and u3 */
+  PetscInt      dof;                          /*number of degrees of freedom at each node, e.g. For 3D linear elasticity: at each node we have u1, u2 and u3 */
   PetscInt      ne;                           /*number of elements to extract per iteration*/
   PetscInt      *conn;
+  PetscInt      sz_conn;
+  PetscInt      sz_perm_idx;
 }AppCtx;
 
 PetscErrorCode processUserOptions(MPI_Comm comm, AppCtx *userOptions);
@@ -21,5 +23,6 @@ PetscErrorCode dmMeshSetup(MPI_Comm comm, AppCtx *user, DM *dm);
 /* Use this function after calling dmMeshSetup.
    It is best to use this function with one element to see its connectivity */
 PetscErrorCode drawOneElem(DM dm,AppCtx *user);
+PetscErrorCode dmExtractElems(DM dm, PetscScalar *u, PetscInt sz_u, PetscInt elem, PetscInt ne, PetscScalar *y);
 
 #endif //end of __USER__H
