110b4a9964e6660e3be595486664a1194bd55a86
ArashMehraban <arashm81@gmail.com>
2017-10-11 22:01:11 -0600

    Removed restric to Q1 and Q2 elemnts functions
    -bug fix about numpads. If numPads == ne to specialaize over then numPad == 0
    -fe is no longer setup in dmCreate.
    -topology is initialized and setup to dm
    -MUST FIX  conn. 8-noded connectivity still have some 0 in them

--- user.h
+++ user.h
@@ -5,14 +5,16 @@
 /*for dm and dmplex objects*/
 #include "fe.h"
 #include "math.h"
+#include "topology.h"
 
 
 typedef struct{
-  char          filename[PETSC_MAX_PATH_LEN]; /*optional exodusII file*/
-  PetscBool	    interpolate;                  /*Generate intermediate mesh points: nodes on faces, edges in addition to vertices and element*/
-  PetscInt      polydegree;                   /*polynomial degree (1 less than number of quadrature points in 1D)*/
-  PetscInt      dof;                          /*number of degrees of freedom at each node, e.g. For 3D linear elasticity: at each node we have u1, u2 and u3 */
-  PetscInt      ne;                           /*number of elements to extract per iteration*/
+  char          filename[PETSC_MAX_PATH_LEN]; //optional exodusII file
+  PetscBool	    interpolate;                  //Generate intermediate mesh points: nodes on faces, edges in addition to vertices and element
+  PetscInt      polydegree;                   //polynomial degree (1 less than number of interpolating points in 1D)
+  PetscInt      addquadpts;                   // Number of additonal quadrature points
+  PetscInt      dof;                          //number of degrees of freedom at each node, e.g. For 3D linear elasticity: at each node we have u1, u2 and u3
+  PetscInt      ne;                           //number of elements to extract per iteration
 }AppCtx;
 
 PetscErrorCode processUserOptions(MPI_Comm comm, AppCtx *userOptions);
@@ -20,6 +22,6 @@ PetscErrorCode dmCreate(MPI_Comm comm, AppCtx user, DM *dm);
 /* Use drawOneElem after calling dmCreate.
    It is best to use this function with one element to see its connectivity */
 PetscErrorCode drawOneElem(DM dm,AppCtx user);
-PetscErrorCode dmRestrictElems(DM dm, const PetscScalar *u, PetscInt elem, PetscInt ne, RestricMode rmode, PetscScalar *y);
+//PetscErrorCode dmRestrictElems(DM dm, const PetscScalar *u, PetscInt elem, PetscInt ne, RestricMode rmode, PetscScalar *y);
 
 #endif //end of __USER__H
